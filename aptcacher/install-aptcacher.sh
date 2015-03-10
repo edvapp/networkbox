d5cc61b52896c6ff772b3a7ab29e4e144478c212
Reinhard Fink <reinhard.fink@tsn.at>
2015-03-10 20:20:21 +0100

    repair install tftpboot

---
+++ aptcacher/install-aptcacher.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# install ng-apt-cacher
+apt-get -y update
+apt-get -y install apt-cacher-ng 
--- tftpboot/install-tftpboot.sh
+++ tftpboot/install-tftpboot.sh
@@ -2,13 +2,13 @@
 
 # install isc-dhcp-server
 apt-get -y update
-apt-get -y tftpd-hpa
+apt-get -y install tftpd-hpa
 
 ## Configuration tftp -server
 # copy files to /var/lib/tftpboot
 cp pxelinux.cfg		/var/lib/tftpboot/
-cp preseed		/var/lib/tftpboot
-cp ubuntu-installer	/var/lib/tftpboot
+cp preseed		/var/lib/tftpboot/
+cp ubuntu-installer	/var/lib/tftpboot/
 
 # download ubuntu - kernel - images 
 /bin/bash wget-Ubuntu.sh
