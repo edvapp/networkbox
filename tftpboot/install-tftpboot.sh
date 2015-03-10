630472c89f61eb8278fa419fc0ad9277d300e7ec
Reinhard Fink <reinhard.fink@tsn.at>
2015-03-10 20:38:32 +0100

    repair cp -R

--- tftpboot/install-tftpboot.sh
+++ tftpboot/install-tftpboot.sh
@@ -6,9 +6,9 @@ apt-get -y install tftpd-hpa
 
 ## Configuration tftp -server
 # copy files to /var/lib/tftpboot
-cp pxelinux.cfg		/var/lib/tftpboot/
-cp preseed		/var/lib/tftpboot/
-cp ubuntu-installer	/var/lib/tftpboot/
+cp -R pxelinux.cfg	/var/lib/tftpboot/
+cp -R preseed		/var/lib/tftpboot/
+cp -R ubuntu-installer	/var/lib/tftpboot/
 
 # download ubuntu - kernel - images 
 /bin/bash wget-Ubuntu.sh
--- tftpboot/wget-Ubuntu.sh
+++ tftpboot/wget-Ubuntu.sh
@@ -4,7 +4,7 @@ FILELIST="initrd.gz linux pxelinux.0"
 
 # download 14.04 i386
 SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-i386/current/images/netboot/ubuntu-installer/i386/"
-DEST="ubuntu-installer/14.04/i386/"
+DEST="/var/lib/tftpboot/ubuntu-installer/14.04/i386/"
 
 for FILE in $FILELIST;
 do
@@ -16,7 +16,7 @@ done
 
 # download 14.04 amd64
 SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
-DEST="ubuntu-installer/14.04/amd64/"
+DEST="/var/lib/tftpboot/ubuntu-installer/14.04/amd64/"
 
 for FILE in $FILELIST;
 do
