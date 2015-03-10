0299ca20cd322c2041a1c0119f5b4d6dff254838
Reinhard Fink <reinhard.fink@tsn.at>
2015-03-10 20:48:42 +0100

    repair pathes in wget-Ubuntu.sh

--- tftpboot/wget-Ubuntu.sh
+++ tftpboot/wget-Ubuntu.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+TFTP_ROOT_PATH=/var/lib/tftpboot
+
 FILELIST="initrd.gz linux pxelinux.0"
 
 # download 14.04 i386
 SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-i386/current/images/netboot/ubuntu-installer/i386/"
-DEST="/var/lib/tftpboot/ubuntu-installer/14.04/i386/"
+DEST=$TFTP_ROOT_PATH"/ubuntu-installer/14.04/i386/"
 
 for FILE in $FILELIST;
 do
@@ -16,7 +18,7 @@ done
 
 # download 14.04 amd64
 SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
-DEST="/var/lib/tftpboot/ubuntu-installer/14.04/amd64/"
+DEST=$TFTP_ROOT_PATH"/ubuntu-installer/14.04/amd64/"
 
 for FILE in $FILELIST;
 do
@@ -27,5 +29,5 @@ do
 done
 
 # copy pxelinux.0 to top - directory
-cp ubuntu-installer/14.04/i386/pxelinux.0 pxelinux.0
+cp $TFTP_ROOT_PATH"/ubuntu-installer/14.04/i386/pxelinux.0" $TFTP_ROOT_PATH
 
