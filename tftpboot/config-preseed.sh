3e382c6dad4288eefcbe918e4763d95433c43ff3
Reinhard Fink <reinhard.fink@tsn.at>
2015-05-10 17:56:31 +0200

    added Ubuntu 15.05 with systemd to tftp

--- tftpboot/config-preseed.sh
+++ tftpboot/config-preseed.sh
@@ -9,7 +9,24 @@
 # generate MD5 - hash from clear text password with "salt" mUxl
 PRESEED_ADMIN_ACCOUNT_PASSWORD_HASH=$(openssl passwd -1 -salt mUxl $PRESEED_ADMIN_ACCOUNT_PASSWORD)
 
-cd /var/lib/tftpboot/preseed
+cd /var/lib/tftpboot/preseed/upstart
+for file in $(ls);
+do
+	# set admin user: fullname
+	sed -e "{
+		/d-i passwd\/user-fullname string/ s/d-i passwd\/user-fullname string/d-i passwd\/user-fullname string $PRESEED_ADMIN_ACCOUNT_FULLNAME/
+	}" -i $file
+ 	# set admin user: name
+	sed -e "{
+		/d-i passwd\/username string/ s/d-i passwd\/username string/d-i passwd\/username string $PRESEED_ADMIN_ACCOUNT_NAME/
+	}" -i $file
+	# set admin user: password - hash
+	sed -e "{
+		/d-i passwd\/user-password-crypted password/ s/d-i passwd\/user-password-crypted password/d-i passwd\/user-password-crypted password $PRESEED_ADMIN_ACCOUNT_PASSWORD_HASH/
+	}" -i $file
+done
+
+cd /var/lib/tftpboot/preseed/systemd
 for file in $(ls);
 do
 	# set admin user: fullname
--- tftpboot/wget-Ubuntu.sh
+++ tftpboot/wget-Ubuntu.sh
@@ -31,3 +31,29 @@ done
 # copy pxelinux.0 to top - directory
 cp $TFTP_ROOT_PATH"/ubuntu-installer/14.04/i386/pxelinux.0" $TFTP_ROOT_PATH
 
+
+# download 15.04 i386
+SOURCE="http://archive.ubuntu.com/ubuntu/dists/vivid/main/installer-i386/current/images/netboot/ubuntu-installer/i386/"
+DEST=$TFTP_ROOT_PATH"/ubuntu-installer/15.04/i386/"
+
+for FILE in $FILELIST;
+do
+	if [ ! -e ${DEST}${FILE} ];
+	then
+		wget -P $DEST ${SOURCE}${FILE}
+	fi
+done
+
+# download 15.04 amd64
+SOURCE="http://archive.ubuntu.com/ubuntu/dists/vivid/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
+DEST=$TFTP_ROOT_PATH"/ubuntu-installer/15.04/amd64/"
+
+for FILE in $FILELIST;
+do
+	if [ ! -e ${DEST}${FILE} ];
+	then
+		wget -P $DEST ${SOURCE}${FILE}
+	fi
+done
+
+
