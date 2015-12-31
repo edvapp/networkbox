29d71cd608096671133e1ffc0b7bae45cbbb2221
Reinhard Fink <reinhard.fink@tsn.at>
2015-12-31 11:00:21 +0100

    reverse order in owncloud install

--- owncloud/uninstall-owncloud.sh
+++ owncloud/uninstall-owncloud.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# uninstall owncloud 
+# uninstall owncloud in reverse order
 
 CURRENT_DIR=$(pwd)
-
-# uninstall mysql-server
-cd ../sql-server
-/bin/bash uninstall-sql_server.sh
-cd $CURRENT_DIR
-
-# uninstall apache
-cd ../web-server
-/bin/bash uninstall-web_server.sh
-cd $CURRENT_DIR
 
 apt-get -y purge owncloud
 
 rm -R /etc/apt/sources.list.d/owncloud.list
 
 ocpath='/var/www/owncloud'
-
 rm -R $ocpath
+
+# uninstall apache
+cd ../web-server
+/bin/bash uninstall-web_server.sh
+cd $CURRENT_DIR
+
+# uninstall mysql-server
+cd ../sql-server
+/bin/bash uninstall-sql_server.sh
+cd $CURRENT_DIR
+
