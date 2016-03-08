6b47e10707968b36dd3a8982b5e2e742c0558f7c
Reinhard Fink <reinhard.fink@tsn.at>
2016-03-08 19:51:46 +0100

    get boot images from trusty-updates instead trusty

--- tftpboot/wget-Ubuntu.sh
+++ tftpboot/wget-Ubuntu.sh
@@ -15,7 +15,7 @@
 FILELIST="initrd.gz linux pxelinux.0"
 
 printAndLogMessage "Download 14.04 i386"
-SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-i386/current/images/netboot/ubuntu-installer/i386/"
+SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/ubuntu-installer/i386/"
 DEST=$TFTP_DIRECTORY"/ubuntu-installer/14.04/i386/"
 
 for FILE in $FILELIST;
@@ -28,7 +28,7 @@ do
 done
 
 printAndLogMessage "Download 14.04 amd64"
-SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
+SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
 DEST=$TFTP_DIRECTORY"/ubuntu-installer/14.04/amd64/"
 
 for FILE in $FILELIST;
