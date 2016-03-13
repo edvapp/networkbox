6fdfa17e3a463b1297cdbcd50da45282fb6bd4d2
Reinhard Fink <reinhard.fink@tsn.at>
2016-03-13 11:03:31 +0100

    added --data-dir option for owncloud installation

--- owncloud/connect_owncloud_and_db.sh
+++ owncloud/connect_owncloud_and_db.sh
@@ -8,4 +8,21 @@
 
 printAndLogMessage "Connect owncloud with database via php occ ..."
 cd /var/www/owncloud/
-sudo -u www-data php occ maintenance:install --database "mysql" --database-name "$DB"  --database-user "$DB_ADMIN" --database-pass "$DB_ADMIN_PASSWORD" --admin-user "$OC_ADMIN" --admin-pass "$OC_ADMIN_PASSWORD"
+
+if  [ "$OC_DATA_PATH" == ""  ];
+then
+	printAndLogMessage "no datapath for owncloud set using default datapath"
+	sudo -u www-data php occ maintenance:install --verbose --database "mysql" --database-name "$DB"  --database-user "$DB_ADMIN" --database-pass "$DB_ADMIN_PASSWORD" --admin-user "$OC_ADMIN" --admin-pass "$OC_ADMIN_PASSWORD"
+else
+	printAndLogMessage "OWN datapath $OC_DATA_PATH for owncloud set"
+	if ! [ -d $OC_DATA_PATH  ];
+	then
+		printAndLogMessage "create directory $OC_DATA_PATH"
+		mkdir $OC_DATA_PATH
+
+	fi
+	printAndLogMessage "set owner and permissions for  $OC_DATA_PATH"
+	chown -R www-data:www-data $OC_DATA_PATH
+	chmod -R 770 $OC_DATA_PATH
+	sudo -u www-data php occ maintenance:install --verbose --database "mysql" --database-name "$DB"  --database-user "$DB_ADMIN" --database-pass "$DB_ADMIN_PASSWORD" --admin-user "$OC_ADMIN" --admin-pass "$OC_ADMIN_PASSWORD" --data-dir "$OC_DATA_PATH"
+fi
