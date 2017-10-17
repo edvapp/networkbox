f814c150746c5cf602b14c129aad8bcbe2234813
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-10-17 15:32:25 +0200

    disable write access to slave DOMAIN database

--- ldap-server/cn_config/config-cn_config.sh
+++ ldap-server/cn_config/config-cn_config.sh
@@ -27,15 +27,18 @@ printAndLogMessage "add sycnmodule to cn=schema,cn=config"
 /bin/bash add_syncmodul_to_cn_config.sh
 
 printAndLogMessage "activate sync-Provider for DOMAIN-database"
-/bin/bash activate_syncmodul_for_DOMAIN_database.sh
+/bin/bash activate-syncmodul_for_DOMAIN_database.sh
 
 printAndLogMessage "encrease maximum for ldap-queries in DOMAIN-database"
-/bin/bash encrease_max_for_queries_in_DOMAIN_database.sh
+/bin/bash encrease-max_for_queries_in_DOMAIN_database.sh
 
 if [ "$LDAP_IS_SLAVE_SERVER" = "yes" ];
 then
     printAndLogMessage "add replication for DOMAIN database to slave cn=schema,cn=config"
-    /bin/bash add_replication_for_DOMAIN_database_to_slave_cn_config.sh
+    /bin/bash add-replication_for_DOMAIN_database_to_slave_cn_config.sh
+
+    printAndLogMessage "disable write to DOMAIN database for slave"
+    /bin/bash disable-write_to_DOMAIN_database_for_slave.sh
 fi
 
 
---
+++ ldap-server/cn_config/disable-write_to_DOMAIN_database_for_slave.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# source helper functions
+. ../../helperfunctions.sh
+
+# source configuration
+. ../../OPTIONS.conf
+
+# set olcRootDN: and olcRootPW: 
+file=./ldif/disable-write_to_DOMAIN_database_for_slave.ldif
+
+echo "
+# disable password change for users
+dn: olcDatabase={1}mdb,cn=config
+changeType: modify
+replace: olcAccess
+olcAccess: {3}to attrs=userPassword,shadowLastChange,krbPrincipalKey,sambaNTPassword,sambaLMPassword by anonymous auth by self read by * none
+
+" > $file
+
+printAndLogMessage "add ldif to cn=config database"
+ldapadd -v -Y EXTERNAL -H ldapi:/// -f $file
