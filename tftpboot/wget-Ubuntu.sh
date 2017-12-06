fda5ff9fd04ff1842dcaeec4d5c20535a02414f1
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-12-06 14:17:49 +0100

    drop UBUNTU 14.04 from tftp menue

--- tftpboot/wget-Ubuntu.sh
+++ tftpboot/wget-Ubuntu.sh
@@ -14,35 +14,6 @@
 
 FILELIST="initrd.gz linux pxelinux.0"
 
-printAndLogMessage "Download 14.04 i386"
-SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/ubuntu-installer/i386/"
-DEST=$TFTP_DIRECTORY"/ubuntu-installer/14.04/i386/"
-
-for FILE in $FILELIST;
-do
-	if [ ! -e ${DEST}${FILE} ];
-	then
-		printAndLogMessage "wget -P "$DEST ${SOURCE}${FILE}
-		wget -P $DEST ${SOURCE}${FILE}
-	fi
-done
-
-printAndLogMessage "Download 14.04 amd64"
-SOURCE="http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
-DEST=$TFTP_DIRECTORY"/ubuntu-installer/14.04/amd64/"
-
-for FILE in $FILELIST;
-do
-	if [ ! -e ${DEST}${FILE} ];
-	then
-		printAndLogMessage "wget -P "$DEST ${SOURCE}${FILE}
-		wget -P $DEST ${SOURCE}${FILE}
-	fi
-done
-
-# copy pxelinux.0 to top - directory
-cp $TFTP_DIRECTORY"/ubuntu-installer/14.04/i386/pxelinux.0" $TFTP_DIRECTORY
-
 
 printAndLogMessage "Download 16.04 amd64"
 SOURCE="http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
@@ -56,6 +27,9 @@ do
 		wget -P $DEST ${SOURCE}${FILE}
 	fi
 done
+
+# copy pxelinux.0 to top - directory
+cp $TFTP_DIRECTORY"/ubuntu-installer/16.04/amd64/pxelinux.0" $TFTP_DIRECTORY
 
 
 printAndLogMessage "Download 17.10 amd64"
