d78dec280b55d5297a977eb0999fe68992937a4c
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2018-03-09 13:44:08 +0100

    remove upstart, 14.04, update *.c32 libraries

--- tftpboot/wget-Ubuntu.sh
+++ tftpboot/wget-Ubuntu.sh
@@ -28,10 +28,6 @@ do
 	fi
 done
 
-# copy pxelinux.0 to top - directory
-cp $TFTP_DIRECTORY"/ubuntu-installer/16.04/amd64/pxelinux.0" $TFTP_DIRECTORY
-
-
 printAndLogMessage "Download 18.04 amd64"
 SOURCE="http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/"
 DEST=$TFTP_DIRECTORY"/ubuntu-installer/18.04/amd64/"
@@ -45,3 +41,8 @@ do
 	fi
 done
 
+printAndLogMessage "copy pxelinux.0 to top - directory"
+cp $TFTP_DIRECTORY"/ubuntu-installer/18.04/amd64/pxelinux.0" $TFTP_DIRECTORY
+
+printAndLogMessage "copy ldlinux.c32 to top - directory"
+cp $TFTP_DIRECTORY"/ubuntu-installer/menus/boot-screens/ldlinux.c32" $TFTP_DIRECTORY
