f8622d44439c001addff0d139a5118a073cbc825
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2020-06-21 17:18:51 +0200

    write krb5.conf on domain member

---
+++ samba4/write-etc_krb5.conf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# source helper functions
+. ../helperfunctions.sh
+
+# source configuration
+. ../OPTIONS.conf
+
+## manipulated file /etc/krb5.conf 
+file=/etc/krb5.conf 
+
+printAndLogMessage "Save original file: " ${file}
+saveOriginal ${file}
+logFile ${file}
+
+rm ${file}
+
+printAndLogMessage "Write file: " ${file}
+
+echo "
+[libdefaults]
+	default_realm = ${SAMBA4_REALM_DOMAIN_NAME}
+	dns_lookup_realm = false
+	dns_lookup_kdc = true
+
+" > ${file}
+
+logFile ${file}
+
+
+
+
