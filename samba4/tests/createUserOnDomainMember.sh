5156ca359c30f69890a9871b5aace94bf23ff7e8
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2020-06-22 17:40:50 +0200

    replace \ wit // for ldap - connection

--- samba4/tests/createUserOnDomainMember.sh
+++ samba4/tests/createUserOnDomainMember.sh
@@ -7,7 +7,7 @@
 . ../../OPTIONS.conf
 
 ## through LDAP - Connection from other machine  
-samba-tool user create zuwl02 Passw0rd --home-drive H: --home-directory \\\\SMB01\\users\\zuwl02 --nis-domain fisc --uid zuwl01 --uid-number 11112 --login-shell /bin/bash --unix-home /home/users/zuwl02 --gid-number 2000 -H ldap:\\ad01.${SAMBA4_DNS_DOMAIN_NAME} -UAdministrator
+samba-tool user create zuwl02 Passw0rd --home-drive H: --home-directory \\\\SMB01\\users\\zuwl02 --nis-domain fisc --uid zuwl01 --uid-number 11112 --login-shell /bin/bash --unix-home /home/users/zuwl02 --gid-number 2000 -H ldap://ad01.${SAMBA4_DNS_DOMAIN_NAME} -UAdministrator
 
 mkdir /home/users/zuwl02
 chmod 700 /home/users/zuwl02
