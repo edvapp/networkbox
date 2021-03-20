7394964d1ab6fc06b51e334ac23a4b2f62d35c6f
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2021-03-20 15:45:59 +0100

    add tls to samba 4

---
+++ samba4/add_tls-etc_samba_smb.conf.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# source helper functions
+. ../helperfunctions.sh
+
+# source configuration
+. ../OPTIONS.conf
+
+## extract certifikate from pkcs12 file, which has to be placed in 
+## /var/lib/samba/private/tls
+
+KEYSTORE_FILE=$(ls *.p12)
+
+file="/var/lib/samba/private/tls/cert.pem"
+printAndLogMessage "Save original file: " ${file}
+saveOriginal ${file}
+logFile ${file}
+
+openssl pkcs12 -in ${KEYSTORE_FILE} -out ${file} -nodes -nokeys
+
+file="/var/lib/samba/private/tls/key.pem"
+printAndLogMessage "Save original file: " ${file}
+saveOriginal ${file}
+logFile ${file}
+
+openssl pkcs12 -in ${KEYSTORE_FILE} -out key.pem -nodes -nocerts
+
+## manipulated file /etc/samba/smb.conf
+file=/etc/samba/smb.conf
+printAndLogMessage "WRITE NEW ${file}"
+printAndLogMessage "Manipulated file: " ${file}
+
+printAndLogMessage  "STOP samba-ad-dc"
+systemctl stop samba-ad-dc
+
+printAndLogMessage "Save original file: " ${file}
+saveOriginal ${file}
+logFile ${file}
+
+printAndLogMessage "Change file: " ${file}
+
+sed -e '/idmap_ldb*/a  \ \ttls enabled = yes \n \ttls keyfile = tls/key.pem \n \ttls certfile = tls/cert.pem \n \ttls cafile  =' -i ${file}
+
+logFile ${file}
+
+printAndLogMessage  "START samba-ad-dc"
+systemctl start samba-ad-dc
+
+
+
+
+
+
