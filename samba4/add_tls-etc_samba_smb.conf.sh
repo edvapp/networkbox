4a62f8f0e47d6c0555f9fc0551a1c9e9bd3c7bf2
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2021-03-21 10:07:49 +0100

    add some checks to ssl/tls integration

--- samba4/add_tls-etc_samba_smb.conf.sh
+++ samba4/add_tls-etc_samba_smb.conf.sh
@@ -6,10 +6,17 @@
 # source configuration
 . ../OPTIONS.conf
 
+
+if [ ! ${ENABLE_SSL} == "yes" ];
+then
+        printAndLogMessage "SSL/TLS will not be enabled"
+        exit
+fi
+
 ## extract certifikate from pkcs12 file, which has to be placed in 
 ## /var/lib/samba/private/tls
 
-KEYSTORE_FILE=$(ls *.p12)
+KEYSTORE_FILE=$(ls /var/lib/samba/private/tls/*.p12)
 
 file="/var/lib/samba/private/tls/cert.pem"
 printAndLogMessage "Save original file: " ${file}
@@ -27,6 +34,12 @@ openssl pkcs12 -in ${KEYSTORE_FILE} -out key.pem -nodes -nocerts
 
 ## manipulated file /etc/samba/smb.conf
 file=/etc/samba/smb.conf
+if grep -q "tls enabled" ${file};
+then
+        printAndLogMessage "SSL/TLS already enabled, we do nothing!"
+        exit
+fi
+
 printAndLogMessage "WRITE NEW ${file}"
 printAndLogMessage "Manipulated file: " ${file}
 
