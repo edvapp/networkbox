e5eb59425403f1c1a22d18a105baab823cb66813
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2021-03-27 17:53:46 +0100

    fixes and  added

--- samba4/add_sync_to_tsn.sh
+++ samba4/add_sync_to_tsn.sh
@@ -13,31 +13,34 @@ then
         exit
 fi
 
-printAndLogMessage "CREATE TSN-SYNC-CONTAINER ${TSN_SYNC_CONTAINER}"
-samba-tool ou create ${TSN_SYNC_CONTAINER}
+printAndLogMessage "CREATE TSN-SYNC-CONTAINER ${OU_TSN_SYNC_CONTAINER}"
+samba-tool ou create ${OU_TSN_SYNC_CONTAINER}
 
 printAndLogMessage "CREATE TSN-SYNC-GROUP ${TSN_SYNC_GROUP}"
-samba-tool group add ${TSN_SYNC_GROUP} --gid-number==${TSN_SYNC_GROUP_GID_NUMBER}
+samba-tool group add ${TSN_SYNC_GROUP} --gid-number=${TSN_SYNC_GROUP_GID_NUMBER} --nis-domain=${SAMBA4_DOMAIN}
 
-printAndLogMessage "move ${TSN_SYNC_GROUP} to ${TSN_SYNC_CONTAINER}"
-samba-tool group move ${TSN_SYNC_GROUP} ${TSN_SYNC_CONTAINER}
+printAndLogMessage "move ${TSN_SYNC_GROUP} to ${OU_TSN_SYNC_CONTAINER}"
+samba-tool group move ${TSN_SYNC_GROUP} ${OU_TSN_SYNC_CONTAINER}
 
-printAndLogMessage "ADD ACCLS TO ${TSN_SYNC_CONTAINER} FOR ${TSN_SYNC_GROUP}"
-## we get full DN= ...
+printAndLogMessage "ADD ACCLS TO ${OU_TSN_SYNC_CONTAINER} FOR ${TSN_SYNC_GROUP}"
 OBJECT_SID_LONG=$(samba-tool group show ${TSN_SYNC_GROUP} --attributes=objectSid)
-## we extract SID, which is last word of the dstring
+## we get something like this:
+## dn: CN=TSN_sync_group,OU=701036,DC=brg,DC=tsn
+## objectSid: S-1-5-21-1101371487-3695870978-1069032044-1103
+## we extract SID, which is last word of the string
 OBJECT_SID=$(echo ${OBJECT_SID_LONG} | awk '{print $NF}')
 ## we build SDDL String
 SDDL="(A;;${ACL_LIST};;;${OBJECT_SID})"
-printAndLogMessage "SDDL ${SDDL} added to object ${TSN_SYNC_CONTAINER}"
-samba-tool dsacl set --objectdn=OU=${TSN_SYNC_CONTAINER} --sddl=${SDDL}
+printAndLogMessage "SDDL ${SDDL} added to object ${OU_TSN_SYNC_CONTAINER}"
+## we need DN for ${OU_TSN_SYNC_CONTAINER} :-(
+samba-tool dsacl set --objectdn=${OU_TSN_SYNC_CONTAINER},${SAMBA4_ROOT_DN}" --sddl=${SDDL}
 
 printAndLogMessage "CREATE TSN-SYNC-USER ${TSN_SYNC_USER}"
-samba-tool group create ${TSN_SYNC_USER} --uid-number=${TSN_SYNC_USER_UID_NUMBER} --gid-number==${TSN_SYNC_USER_GID_NUMBER}
+samba-tool user create ${TSN_SYNC_USER} --uid-number=${TSN_SYNC_USER_UID_NUMBER} --gid-number=${TSN_SYNC_USER_GID_NUMBER}
 
 printAndLogMessage "add ${TSN_SYNC_USER} to ${TSN_SYNC_GROUP}"
 samba-tool group addmembers ${TSN_SYNC_GROUP} ${TSN_SYNC_USER}
 
-printAndLogMessage "move ${TSN_SYNC_USER} to ${TSN_SYNC_CONTAINER}"
-samba-tool user move ${TSN_SYNC_USER} ${TSN_SYNC_CONTAINER}
+printAndLogMessage "move ${TSN_SYNC_USER} to ${OU_TSN_SYNC_CONTAINER}"
+samba-tool user move ${TSN_SYNC_USER} ${OU_TSN_SYNC_CONTAINER}
 
