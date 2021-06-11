af81253ac94178992b3d287aaf3ae382ea91f291
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2021-06-11 11:54:37 +0200

    check for missing homedirs for testees

--- samba4/helper_scripts/check-for_missing_homedirs.sh
+++ samba4/helper_scripts/check-for_missing_homedirs.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
+## directory to save unused homedirectories
+SAVED_OLD_HOMEDIRS="zzzOLD"
+
 USER_LIST=$(wbinfo -u)
 
 for USER in ${USER_LIST};
 do
         ## get user info in format
-        ## DOMAIN\username:*:uidNUmber:gidNumber::homedirectory_path:/bin/bash
+        ## username:*:uidNUmber:gidNumber::homedirectory_path:/bin/bash
         echo "processing ${USER}"
         wbinfo --user-info ${USER}
         ## check if $? <=> exit-status of wbinfo --user-info is 0 <=> success
         ## because wbinfo --user-info does not work for build in users like administrator 
         if [ "$?" == "0" ];
         then
+                ## we get homedirpath from wbinfo
                 USER_HOME_DIR_PATH=$(wbinfo --user-info ${USER} | awk 'BEGIN {FS = ":" } { print $6 } ')
+                ## we have to handle test accounts seperate
+                ## because homedir looks like /tmp/testee_701036_001
+                if [ "$(dirname ${USER_HOME_DIR_PATH})" == "/tmp" ];
+                then
+                        ## we extract SCHOOLNUMBER from username testee_701036_001
+                        SCHOOL_NUMBER=${USER:7:6}
+                        ## we build new new USER_HOME_DIR_PATH 
+                        ## which is for Windows
+                        USER_HOME_DIR_PATH="/home/users/${SCHOOL_NUMBER}/t/${USER}"
+                fi
                 echo "checking ${USER_HOME_DIR_PATH} for ${USER}"
                 if [ ! -d ${USER_HOME_DIR_PATH} ];
                 then
+                        
                         echo "creating homedir ${USER_HOME_DIR_PATH} for ${USER}"
                         USER_UID_NUMBER=$(wbinfo --user-info ${USER} | awk 'BEGIN {FS = ":" } { print $3 } ')
                         USER_GID_NUMBER=$(wbinfo --user-info ${USER} | awk 'BEGIN {FS = ":" } { print $4 } ')
